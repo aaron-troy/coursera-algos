d26a211c5822854570449dbcbc601322114e956e
aaron-troy <aaron.troy@mail.utoronto.ca>
2023-04-27 10:43:39 -0400

    greedy difference and quotient

--- course_3_greedy_algos/Programming_assignment_1a_jobScheduling/greedyScheduling.py
+++ course_3_greedy_algos/Programming_assignment_1a_jobScheduling/greedyScheduling.py
@@ -1,10 +1,12 @@
-
+"""
+Greedy algorithms for scheduling a list of input tasks, each with an associated weight and length.
+"""
 import time
-
-
 def read_file(source : str):
     """
-    Helper function for reading the input file, returned as an array
+    Helper function for reading the input file, returned as a dict unique ids to job weights and lengths.
+    The input is a text file, with the first line containing the number of jobs.
+    Each subsequent line has 2 integers, corresponding the job weight and length.
     """
     D = {}
     with open(source) as file:
@@ -14,17 +16,65 @@ def read_file(source : str):
             i += 1
     return D
 
-def greedy_difference(J : dict):
-    return 0
+def greedy_difference_schedule(J : dict):
+    """
+    Implementation of greedy scheduling algorithm, scheduling based on difference between task
+    weight and length. Computes the total weighted completion time for the resulting schedule.
+    Result is not optimal.
+    """
+    # Assemble a list of tuples, sorted by decreasing difference, then weight
+    sch = []
+    for i, (w,l) in J.items():
+        sch.append((i,w,l,w-l))
+    sch_sorted = sorted(sch, key=lambda x: (-x[3], -x[1]))
+
+    # Compute the total weighted completion time
+    compTimes = []
+    t = 0
+    for i, w, l, _ in sch_sorted:
+        compTimes.append(w * (l + t))
+        t += l
+    return sum(compTimes)
+
+def greedy_quotient_schedule(J : dict):
+    """
+    Implementation of greedy scheduling algorithm, scheduling based on decreasing weight / lenght quotient
+    Computes the total weighted completion time for the resulting schedule. Result is not optimal.
+    """
+    # Assemble a list of tuples, sorted by decreasing difference, then weight
+    sch = []
+    for i, (w, l) in J.items():
+        sch.append((i, w, l, w / l))
+    sch_sorted = sorted(sch, key=lambda x: (-x[3]))
+
+    # Compute the total weighted completion time
+    compTimes = []
+    t = 0
+    for i, w, l, _ in sch_sorted:
+        compTimes.append(w * (l + t))
+        t += l
+    return sum(compTimes)
+
 
 if __name__ =="__main__":
 
     # Source file for the input graph
     source = "jobList.txt"
-    # Build the graph
+    # Read the input
     jobs = read_file(source)
-    print(jobs.values())
     # Timing
     begin = time.time()
+    # Compute greedy scheduling using greedy difference
+    compTime = greedy_difference_schedule(jobs)
+    print('Greedy difference ran in', time.time() - begin, "seconds")
+    print('Total weighted completion time:', compTime)
+
+
+    # Timing
+    begin = time.time()
+    # Compute greedy scheduling using greedy quotient
+    compTime = greedy_quotient_schedule(jobs)
+    print('Greedy quotient ran in', time.time() - begin, "seconds")
+    print('Total weighted completion time:', compTime)
 
 
