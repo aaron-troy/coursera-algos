22a11f2664d4bb6fac6d1c9990afbcad95caaa87
aaron-troy <aaron.troy@mail.utoronto.ca>
2023-04-30 17:47:16 -0400

    added Kruskal's algo with lazy union find

--- course_3_greedy_algos/Programming_assignment_1b_PrimsMST/PrimsMST.py
+++ course_3_greedy_algos/Programming_assignment_1b_PrimsMST/PrimsMST.py
@@ -1,12 +1,15 @@
 """
 Computing the minimum spanning tree (MST) from an undirected weight graph. Input is a text file
-adjacency list with weights, output the cost of the MST (sum of weights) compute using Prim's greedy
-algorithm.
+adjacency list with weights, output the cost of the MST (sum of weights). Computation is performed twice,
+using two different greedy algorithms. The first is Prim's algorithm, implemented using a heap data structure.
+The second is Kruskal's algorithm, which deploys a lazy union-find by rank data structure. Both approaches
+achieve O(mlog(n)) running time.
 """
 
 import time
 import graphs
 import heap
+import union_find
 
 def read_file(source : str):
     """
@@ -29,7 +32,7 @@ def build_graph(adj_list: list):
         G.add_edge(x,y,w)
     return G
 
-def Prims_MST(G : graphs.Graph):
+def Prim_MST(G : graphs.Graph):
     """
     Compute the minimum spanning tree (MST) for an input undirected graph using Prim's greedy algo.
     Implemented using min-heap data structure. At each step, edges crossing the frontier from explored
@@ -37,7 +40,7 @@ def Prims_MST(G : graphs.Graph):
     the root and see if it goes somewhere unexplored. If yes, add it to the MST and update the heap with
     new frontier edges.
 
-    This implementation avoid having to delete arbitrary elements from the heap, but the heap will
+    This implementation avoids having to delete arbitrary elements from the heap, but the heap will
     contain redundant edges as a consequence.
     """
     # Initialize heap for frontier edges and get the start vertex
@@ -65,6 +68,45 @@ def Prims_MST(G : graphs.Graph):
                     H.insert(add_weight, (nxt, add))
     return T
 
+def Kruskal_MST(G : graphs.Graph):
+    """
+    Compute the minimum spanning tree (MST) for an input undirected graph using Kruskal's greedy algo
+    -- an alternative to Prim's above. Implemented using union-find data structure. In this approach,
+    we traverse a sorted list of edges increasing in cost. We greedily add each successive edge to the
+    MST if its addition does not result in a directed cycle.
+
+    Deploying the UF data structure allows for "blazingly fast" checks for these cycles, speeding things
+    up from an O(n^2) running time to O(mlog(n)).
+    """
+
+    # Get a list of edges and sort
+    edges = sorted(G.edges, key= lambda x: x[2])
+
+    # Use a graph object for the MST
+    T = graphs.Graph()
+
+    # Union-find object to accelerate Kruskal
+    U = union_find.UnionFind()
+    for m1, m2, _ in edges:
+        U.add_member(m1)
+        U.add_member(m2)
+
+    # Continue merging parents until we span the graph
+    while len(set(U.parents.values())) > 1:
+        # Take the cheapest edge
+        if len(edges) < 1:
+            print('Graph is not connected. MST not possible')
+            break
+        start, end, cost = edges.pop(0)
+        # If the edge connects distinct subtrees, and thus doesn't induce a cycle, add it to the MST
+        if U.find(start) != U.find(end):
+            T.add_edge(start, end, cost)
+            # Update the union-find
+            U.union(start, end)
+
+    return T
+
+
 
 if __name__ == "__main__":
 
@@ -76,8 +118,20 @@ if __name__ == "__main__":
     # Timing
     begin = time.time()
 
-    # Compute MST
-    MST = Prims_MST(G)
+    # Compute MST, first using Prim's algorithm
+    MST_Prims = Prim_MST(G)
 
     print("Prim's MST ran in", time.time() - begin, "seconds")
-    print("MST cost:", MST.edge_sum)
+    print("MST cost:", MST_Prims.edge_sum)
+
+    # Re-build the graph to recompute the MST using Kruskal's algorithm
+    G = build_graph(read_file(source))
+
+    # Timing
+    begin = time.time()
+
+    # Compute MST again, this time using Kruskal's algorithm
+    MST_Kruskal = Kruskal_MST(G)
+
+    print("Kruskal's MST ran in", time.time() - begin, "seconds")
+    print("MST cost:", MST_Kruskal.edge_sum)
