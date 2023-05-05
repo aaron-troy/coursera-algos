f6af46c57f613e01a401fda6c0fad08de090e568
aaron-troy <aaron.troy@mail.utoronto.ca>
2023-05-04 20:36:56 -0400

    script running Huffman encoding

--- course_3_greedy_algos/Programming_assignment_3a_Huffman/huffman_encoding.py
+++ course_3_greedy_algos/Programming_assignment_3a_Huffman/huffman_encoding.py
@@ -1,64 +1,23 @@
+"""
+Huffman encoding implemented using min heap.
+
+Completed for programming assignment 3 of course 3 of the Coursera algorithms
+specialization by Tim Roughgarden
+"""
+
 import time
-import heap
+import huffman as hf
 
-class HuffmanNode:
-    def __init__(self, id, weight):
-        self.id = id
-        self.weight = weight
-        self.left_child = None
-        self.right_child = None
-        self.parent = None
-        self.code = ''
-
-class HuffmanTree:
-
-    def __init__(self):
-        self.nodes = {}
-        self.leaves = {}
-        self.codes = {}
-        self.min_heap = heap.MinHeap()
-        self.size = 0
-        self.root = None
-
-    def insert(self, key, weight: int, leaf: bool = False):
-        if key not in self.nodes:
-            self.nodes[key] = HuffmanNode(key, weight)
-            self.min_heap.insert(weight, key)
-            self.size += 1
-            # Add to leaves if desired
-            if leaf:
-                self.leaves[key] = key
-
-    def build_encoding(self):
-
-        i = max(self.nodes.keys())
-        while self.min_heap.heap_size > 1:
-            i += 1
-            # Get next two weights for the children
-            right_weight, right_child = self.min_heap.pop_root()
-            left_weight, left_child = self.min_heap.pop_root()
-
-            # Create a new node that is parent to the two lowest weight nodes in the heap
-            self.insert(i, left_weight + right_weight)
-            self.nodes[i].right_child = right_child
-            self.nodes[i].left_child = left_child
-
-        self.root = i
-    def generate_codes(self, root_key, code: str= ''):
-        """
-        Recursively work through a Huffman tree to assign codes
-        """
-
-        self.nodes[root_key].code = code
-        self.codes[root_key] = code
-
-        if (self.nodes[root_key].left_child is None) and (self.nodes[root_key].right_child is None):
-            return None
-        else:
-            self.generate_codes(self.nodes[root_key].left_child, code=self.codes[root_key] + '0')
-            self.generate_codes(self.nodes[root_key].right_child, code=self.codes[root_key] + '1')
-        return None
 def read_input(src : str):
+    """
+    Helper function to read input txt file. First line the number of entries.
+    Subsequent lines are the symbol weights
+    Args:
+        src: str, file path for the input
+
+    Returns: list of symbol weights
+
+    """
     with open(src) as file:
         inp = []
         for line in file.readlines()[1:]:
@@ -68,9 +27,9 @@ def read_input(src : str):
 def huffman_encode(weights: list):
 
     # Initialize Huffman tree
-    HT = HuffmanTree()
+    HT = hf.HuffmanTree()
     for i, w in enumerate(weights):
-        HT.insert(i, w, leaf=True)
+        HT.add_node(i, w, leaf=True)
 
     # Build the encoding
     HT.build_encoding()
@@ -82,19 +41,6 @@ def huffman_encode(weights: list):
     leaf_codes = {key : HT.codes[key] for key in HT.leaves}
 
     return leaf_codes
-
-def generate_huffman_coding(tree: dict, root: HuffmanNode, code: str = ''):
-    """
-    Recursively work through a Huffman tree to assign codes
-    """
-    root.code = code
-
-    if (root.left_child is None) and (root.right_child is None):
-        return None
-    else:
-        generate_huffman_coding(tree, tree[root.left_child], code=root.code + '0')
-        generate_huffman_coding(tree, tree[root.right_child], code=root.code + '1')
-    return None
 
 
 if __name__ == "__main__":
