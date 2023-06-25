1dc8bb9e770afe8a8ba170528cb95af7ea0dac3b
aaron-troy <aaron.troy@mail.utoronto.ca>
2023-06-25 18:14:30 -0400

    updated docstring format to google std

--- course_2_graph_search/Programming_assignment_2_dijkstra/graphs.py
+++ course_2_graph_search/Programming_assignment_2_dijkstra/graphs.py
@@ -5,18 +5,24 @@ class Vertex:
     def __init__(self, key):
         """
         Constructor
-        :param key: unique, hashable identifier for Vertex
+
+        Args:
+            key: unique, hashable identifier for Vertex
         """
         self.id = key
         self.explored = False
         self.neighbors = {}
 
-    def add_neighbor(self, key, weight=0):
+    def add_neighbor(self, key, weight: float = 0):
         """
         Add a neighbor
-        :param key: nique, hashable identifier for the neighbour Vertex
-        :param weight: weight of the edge, optional
-        :return: None
+
+        Args:
+            key: unique, hashable identifier for the neighbour Vertex
+            weight: float, weight of the edge, optional
+
+        Returns:
+            None
         """
         self.neighbors[key] = weight
         return None
@@ -38,30 +44,40 @@ class Graph:
     def get_vertices(self):
         """
         Get the keys of all Vertex objects in the graph
-        :return: keys of all Vertex objects
+
+        Returns:
+             dict.keys of all Vertex objects
         """
         return self.verts.keys()
 
     def add_vert(self, key):
         """
         Add a Vertex to the graph
-        :param key: unique, hashable identifier for the Vertex object to create and add
-        :return: None
+
+        Args:
+            key: unique, hashable identifier for the Vertex object to create and add
+
+        Returns:
+            None
         """
         if key not in self.verts:
             self.verts[key] = Vertex(key)
             self.num_verts += 1
         return None
-    def add_edge(self, start : int, end :  int, weight=0):
+
+    def add_edge(self, start: int, end: int, weight: float = 0):
         """
         Add a weighted edge connecting two Vertex objects in the graph. Start and/or end are created and added
         if not in the graph already
-        :param start: unique, hashable identifier for the edge start Vertex object
-        :param end: unique, hashable identifier for the edge end Vertex object
-        :param weight: weight of the edge, optional
-        :return: None
-        """
 
+        Args:
+            start: unique, hashable identifier for the edge start Vertex object
+            end: unique, hashable identifier for the edge end Vertex object
+            weight: float, weight of the edge, optional
+
+        Returns:
+            None
+        """
         if start not in self.verts:
             self.add_vert(start)
         if end not in self.verts:
