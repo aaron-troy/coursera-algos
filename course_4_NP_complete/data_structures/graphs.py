d29d102d0c43150d89944645c80adbeb1a2db95b
aaron-troy <aaron.troy@mail.utoronto.ca>
2023-06-28 21:56:00 -0400

    docstring to Google std

--- course_4_NP_complete/data_structures/graphs.py
+++ course_4_NP_complete/data_structures/graphs.py
@@ -6,7 +6,8 @@ class Vertex:
     def __init__(self, node):
         """
         Constructor for vertex.
-        :param node: unique, hashable identifier for the vertex.
+        Args:
+            node: unique, hashable identifier for the vertex.
         """
         # Unique hashable identifier
         self.id = node
@@ -19,12 +20,16 @@ class Vertex:
         # Dict for downstream neighbour vertices
         self.downstream = {}
 
-    def add_downstream(self, neighbor, weight=0):
+    def add_downstream(self, neighbor, weight: float = 0):
         """
         Add a weighted link to a downstream vertex
-        :param neighbor: hashable identifier for the downstream vertex
-        :param weight: optional weight for the link, default to 0
-        :return: None
+
+        Args:
+            neighbor: hashable identifier for the downstream vertex
+            weight: float, optional weight for the link, default to 0
+
+        Returns:
+            None
         """
         self.downstream[neighbor] = weight
         return None
@@ -32,14 +37,17 @@ class Vertex:
     def get_connections(self):
         """
         Return the keys for downstream neighbour vertices
-        :return: self.downstream.keys(), dict keys for linked downstream vertices
+
+        Returns:
+            self.downstream.keys(): dict keys for linked downstream vertices
         """
         return self.downstream.keys()
 
     def explore(self):
         """
         Update explored flag to true
-        :return:None
+        Returns:
+            None
         """
         self.explored = True
         return None
@@ -47,17 +55,25 @@ class Vertex:
     def set_leader(self, leader):
         """
         Set the leader for a vertex, indicating its SCC membership
-        :param leader: Hashable identifier for the leader
-        :return: None
+
+        Args:
+            leader: Hashable identifier for the leader
+
+        Returns:
+            None
         """
         self.leader = leader
         return None
 
-    def set_order(self, order):
+    def set_order(self, order: int):
         """
         Set place in the search order for the 2nd pass of Kosaraju's algo
-        :param order: Place in search order
-        :return: None
+
+        Args:
+            order: int, index in search order
+
+        Returns:
+            None
         """
         self.order = order
         return None
@@ -79,8 +95,12 @@ class Graph:
     def add_vert(self, node):
         """
         Add a vertex to the graph. Generates a new vertex instance.
-        :param node: Hashable identifier for the vertex to add.
-        :return: None
+
+        Args:
+            node: Hashable identifier for the vertex to add.
+
+        Returns:
+            None
         """
         if node not in self.verts:
             self.verts[node] = Vertex(node)
@@ -90,8 +110,12 @@ class Graph:
     def remove_vert(self, node):
         """
         Remove a vertex from the graph.
-        :param node: Unique identifier for the vertex to remove. Must be in the graph.
-        :return: None
+
+        Args:
+            node: Unique identifier for the vertex to remove. Must be in the graph.
+
+        Returns:
+            None
         """
         if node in self.verts:
             self.verts.pop(node)
@@ -100,16 +124,21 @@ class Graph:
     def get_vertices(self):
         """
         Get keys for all vertices in the graph
-        :return: self.verts.keys, keys for vertices in the graph
+
+        Returns:
+            self.verts.keys: keys for vertices in the graph
         """
         return self.verts.keys()
 
     def get_vert(self, node):
         """
         Get Vertex object by identifier
-        :param node: Unique, hashable identifier for the vertex
-        :return: self.verts[node], Vertex object corresponding the passsed identifier.
-                None if desired vertex not in graph
+
+        Args:
+            node: Unique, hashable identifier for the vertex
+
+        Returns:
+            self.verts[node]: Vertex object corresponding the passed identifier. None if desired vertex not in graph
         """
         if node in self.verts:
             return self.verts[node]
@@ -118,18 +147,23 @@ class Graph:
 
     def get_leaders(self):
         """
-        Get a dict of the leaders for all vertices in the graph
-        :return:
+            Get a dict of the leaders for all vertices in the graph
+        Returns:
+            dict: n : self.verts[].leader
         """
         return {n: self.verts[n].leader for n in self.verts}
 
-    def add_edge(self, start, end, weight=0):
+    def add_edge(self, start, end, weight: float =0):
         """
         Add a weighted, directed edge to the graph. Add Vertex instances for the start and end if not already in the graph
-        :param start: unique, hashable identifier for the start Vertex
-        :param end: unique, hashable identifier for the end Vertex
-        :param weight: weight for the edge, optional, default 0
-        :return: None
+
+        Args:
+            start: unique, hashable identifier for the start Vertex
+            end: unique, hashable identifier for the end Vertex
+            weight: float, weight for the edge, optional, default 0
+
+        Returns:
+            None
         """
         if start not in self.verts:
             self.add_vert(start)
